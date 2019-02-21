f99d928e523ce5b47b6250360e858cb579ebc75e
rteina <rteina@galigeo.com>
2019-02-21 12:15:56 +0100

    Cleaning starter App
    Cleaning Heroku NodeJS Starter App. Initiate index and mobile views

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
+'use strict';
+
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express();
+
+app.disable('etag');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// [END enable_parser]
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/mobile', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/mobile.html'));
+});
+
+
+
+// Basic 404 handler
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use((err, req, res) => {
+  /* jshint unused:false */
+  console.error(err);
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
+});
+
+app
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+/*
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
